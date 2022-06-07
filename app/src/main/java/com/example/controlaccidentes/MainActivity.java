4181338164234ba1c4adcb14ba2c9cfcf7ea8796
moises6 <moisesviera30@gmail.com>
2022-06-06 20:37:49 -0600

    registroIncident

--- app/src/main/java/com/example/controlaccidentes/MainActivity.java
+++ app/src/main/java/com/example/controlaccidentes/MainActivity.java
@@ -6,15 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton accidents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        accidents.findViewById(R.id.imageButtonAccident);
+
+        accidents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, registroInsidente.class);
+
+            }
+        });
 
         Button cerrarSension = findViewById(R.id.bntCerrarSencion);
         cerrarSension.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/controlaccidentes/registroInsidente.java
+++ app/src/main/java/com/example/controlaccidentes/registroInsidente.java
@@ -2,13 +2,53 @@ package com.example.controlaccidentes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class registroInsidente extends AppCompatActivity {
+    EditText editTextFecha;
+    Calendar   calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_insidente);
+
+        editTextFecha = findViewById(R.id.editTimefecha);
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener  date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                updateCalendar();
+            }
+            private void updateCalendar() {
+                String Format = "MM/DD/YY";
+                SimpleDateFormat sdf = new SimpleDateFormat(Format, Locale.US);
+
+                editTextFecha.setText(sdf.format(calendar.getTime()));
+            }
+        };
+        editTextFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(registroInsidente.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+        });
     }
+
+
 }
